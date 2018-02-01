70263786ecf84655e65594bdca76da7ed4ab8653
kipa <pavlov.cpp@gmail.com>
2018-02-01 20:48:57 +0300

    Added first version of template

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+function changeBg1() {
+    document.getElementById("slider").style.backgroundImage = "url('img/slider-one.jpg')";
+    document.getElementById("thumb2").style.boxShadow = "none";
+    document.getElementById("thumb3").style.boxShadow = "none";
+    document.getElementById("thumb4").style.boxShadow = "none";
+    document.getElementById("thumb1").style.boxShadow = "inset 0 0 0 7px rgb(189, 27, 27)";
+}
+
+function changeBg2() {
+    document.getElementById("slider").style.backgroundImage = "url('img/slider-two.jpg')";
+    document.getElementById("thumb1").style.boxShadow = "none";
+    document.getElementById("thumb3").style.boxShadow = "none";
+    document.getElementById("thumb4").style.boxShadow = "none";
+    document.getElementById("thumb2").style.boxShadow = "inset 0 0 0 7px rgb(189, 27, 27)";
+}
+
+function changeBg3() {
+    document.getElementById("slider").style.backgroundImage = "url('img/slider-three.jpg')";
+    document.getElementById("thumb1").style.boxShadow = "none";
+    document.getElementById("thumb2").style.boxShadow = "none";
+    document.getElementById("thumb4").style.boxShadow = "none";
+    document.getElementById("thumb3").style.boxShadow = "inset 0 0 0 7px rgb(189, 27, 27)";
+}
+
+function changeBg4() {
+    document.getElementById("slider").style.backgroundImage = "url('img/slider-four.jpg')";
+    document.getElementById("thumb1").style.boxShadow = "none";
+    document.getElementById("thumb2").style.boxShadow = "none";
+    document.getElementById("thumb3").style.boxShadow = "none";
+    document.getElementById("thumb4").style.boxShadow = "inset 0 0 0 7px rgb(189, 27, 27)";
+}
